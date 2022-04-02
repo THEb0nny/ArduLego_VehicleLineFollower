9bff16c157ad3eb1965cadf54de430544f390a27
Dmitriy Antipov <aston_bonny@mail.ru>
2022-04-03 02:23:47 +0300

    03_04_22_1

--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,6 +1,8 @@
 // https://www.youtube.com/watch?v=jW3cY4Irs50
+// https://alexgyver.ru/gyverpid/
 
 #include <Servo.h>
+#include "GyverPID.h"
 
 #define SERVO_MOT_L_PIN 2
 #define SERVO_MOT_R_PIN 4
@@ -8,6 +10,8 @@
 #define RESET_BTN_PIN D4
 
 Servo lServoMot, rServoMot;
+
+GyverPID regulator(0.1, 0.05, 0.01, 10);
 
 void(* resetFunc) (void) = 0; // Soft reset function
 
@@ -18,6 +22,8 @@ void setup() {
   rServoMot.attach(SERVO_MOT_R_PIN);
   motorSpeed(lServoMot, 0);
   motorSpeed(rServoMot, 0);
+  regulator.setDirection(NORMAL); // направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
+  regulator.setLimits(0, 255);    // пределы (ставим для 8 битного ШИМ). ПО УМОЛЧАНИЮ СТОЯТ 0 И 255
 }
 
 void loop() {
