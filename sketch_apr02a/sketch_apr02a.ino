2d7baebabbf7d5fe5d615759e0c866bb6da13322
Dmitriy Antipov <aston_bonny@mail.ru>
2022-04-03 18:09:44 +0300

    03_04_22_2

--- sketch_apr02a/sketch_apr02a.ino
+++ sketch_apr02a/sketch_apr02a.ino
@@ -1,39 +1,56 @@
 // https://www.youtube.com/watch?v=jW3cY4Irs50
 // https://alexgyver.ru/gyverpid/
 
+#include <SoftwareSerial.h>
 #include <Servo.h>
 #include "GyverPID.h"
 
-#define SERVO_MOT_L_PIN 2
-#define SERVO_MOT_R_PIN 4
-
-#define RESET_BTN_PIN D4
+#define SERVO_MOT_L_PIN 2 // Пин левого серво мотора
+#define SERVO_MOT_R_PIN 4 // Пин правого серво мотора
+#define RESET_BTN_PIN 7 // Кнопка для мягкого перезапуск
 
 Servo lServoMot, rServoMot;
 
-GyverPID regulator(0.1, 0.05, 0.01, 10);
+GyverPID regulator(1, 0, 0, 10); // Инициализируем коэффициенты регулятора
 
-void(* resetFunc) (void) = 0; // Soft reset function
+void(* softResetFunc) (void) = 0; // Soft reset function
 
 void setup() {
   Serial.begin(9600);
-  //pinMode(RESET_BTN, INPUT_PULLUP); // start/stop/reset button attachment
+  Serial.setTimeout(100);
+  pinMode(RESET_BTN_PIN, INPUT_PULLUP); // Start/stop/reset button attachment
   lServoMot.attach(SERVO_MOT_L_PIN);
   rServoMot.attach(SERVO_MOT_R_PIN);
-  motorSpeed(lServoMot, 0);
-  motorSpeed(rServoMot, 0);
-  regulator.setDirection(NORMAL); // направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
-  regulator.setLimits(0, 255);    // пределы (ставим для 8 битного ШИМ). ПО УМОЛЧАНИЮ СТОЯТ 0 И 255
+  //motorSpeed(lServoMot, 0); // При старте моторы выключаем
+  //motorSpeed(rServoMot, 0);
+  lServoMot.write(90);
+  rServoMot.write(90);
+  regulator.setDirection(NORMAL); // Направление регулирования (NORMAL/REVERSE). ПО УМОЛЧАНИЮ СТОИТ NORMAL
+  regulator.setLimits(0, 255); // Пределы регулятора
 }
 
 void loop() {
-  //if (!digitalRead(RESET_BTN)) resetFunc();
-  motorSpeed(lServoMot, 0);
+  if (!digitalRead(RESET_BTN_PIN)) softResetFunc(); // Если клавиша нажата, то сделаем мягкую перезагрузку
+  regulator.setpoint = 0; // Передаём ошибку
+  float u = regulator.getResultTimer();
+  MotorsControl(-30, 50);
 }
 
-void motorSpeed(Servo servoMot, int speed) {
+void MotorsControl(int dir, byte speed) {
+  int lServoMotSpeed = speed + dir, rServoMotSpeed = speed - dir;
+  int z = speed / max(abs(lServoMotSpeed), abs(rServoMotSpeed)); // Вычисляем отношение желаемой мощности к наибольшей фактической
+  lServoMotSpeed *= z, rServoMotSpeed *= z;
+  MotorSpeed(lServoMot, lServoMotSpeed); MotorSpeed(rServoMot, rServoMotSpeed);
+}
+
+void MotorSpeed(Servo servoMot, int speed) {
   // Servo, 0->FW, 90->stop, 180->BW
   speed = constrain(speed, -90, 90);
-  speed = map(speed, 0, 180, -90, 90);
+  if (speed >= 0) {
+    speed = map(speed, 0, 90, 90, 180);
+  } else {
+    speed = map(speed, 0, -90, 90, 0);
+  }
+  Serial.println(speed);
   servoMot.write(speed);
 }
